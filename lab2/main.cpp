0c18a53bfbbd15176519d9bc991d4c123a507671
Robert Fitzgerald <robfitzgerald@gmail.com>
2015-07-05 13:59:09 -0600

    Initial Commit

---
+++ lab2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab2
+//
+//  Created by Robert Fitzgerald on 7/5/15.
+//  Copyright (c) 2015 Robert Fitzgerald. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
